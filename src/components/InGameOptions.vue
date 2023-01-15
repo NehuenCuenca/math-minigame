e199b9fb3e12e1321c9c124320dea562cd215d95
NehuenCuenca <nehuen.cuenca@hotmail.com>
2023-01-15 13:27:49 -0300

    again, add InGameOptions

---
+++ src/components/InGameOptions.vue
@@ -0,0 +1,7 @@
+<template>
+    <ul>
+      <li class="option" @click="$emit('receiveGameOption', 'startNewGame')">Jugar otra vez</li>
+      <li class="option" @click="$emit('receiveGameOption', 'goToMenu')">Ir al menu</li>
+    </ul>
+</template>
+
